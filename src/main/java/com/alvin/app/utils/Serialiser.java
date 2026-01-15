b28399458069f53fba6b48376f55db2627548248
Alvin <alvinkuriaakose@gmail.com>
2026-01-15 23:51:24 +0530

    serialising of class implemented

--- src/main/java/com/alvin/app/utils/Serialiser.java
+++ src/main/java/com/alvin/app/utils/Serialiser.java
@@ -1,5 +1,20 @@
 package com.alvin.app.utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Serialiser {
+    public static <T> void writeClass(T obj, Path path){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String serialised = mapper.writeValueAsString(obj);
+            Files.writeString(path ,serialised);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
